5c833a35bb7e17f70d162d0f0e44a906a6b0f96c
Y-rog <fulgueiras.gregory@gmail.com>
2024-02-22 19:24:14 +0100

    add front header footer nav and home-page

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class HomeController extends AbstractController
+{
+    #[Route('/', name: 'page_home')]
+    public function home(): Response
+    {
+        return $this->render('home/index.html.twig', [
+            'controller_name' => 'HomeController',
+            'title_page' => 'Bienvenue au zoo',
+        ]);
+    }
+}
